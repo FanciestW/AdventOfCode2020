eaa31509f86f9a8055d1c7ca1dfd841a7a1cb6af
William Lin <wlin26@yahoo.com>
2020-12-03 14:51:07 -0500

    Completed Day_3 part 2 challenge

--- Day_3/main.py
+++ Day_3/main.py
@@ -1,4 +1,5 @@
 import os
+import math
 
 def read_input(file_name: str) -> list:
     try:
@@ -26,6 +27,32 @@ def check_trees(data: list) -> int:
         if (j >= len(data[i])):
             j -= len(data[i])
     return count
-    
+
+def check_trees_v2(data: list, right_count: int, down_count: int) -> int:
+    count = 0
+    i = 0
+    j = 0
+    while(i < len(data)):
+        if data[i][j] == '#':
+            count += 1
+        i += down_count
+        j += right_count
+        if (i >= len(data)): break
+        if (j >= len(data[i])):
+            j -= len(data[i])
+    return count
+
+# Part 1
 data = read_input(os.path.join(os.path.dirname(__file__), 'input.txt'))
 print(check_trees(data))
+# > 244
+
+# Part 2
+res_list = list()
+res_list.append(check_trees_v2(data, 1, 1))
+res_list.append(check_trees_v2(data, 3, 1))
+res_list.append(check_trees_v2(data, 5, 1))
+res_list.append(check_trees_v2(data, 7, 1))
+res_list.append(check_trees_v2(data, 1, 2))
+print(math.prod(res_list))
+# > 9406609920
