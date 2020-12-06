5269b78c10fe5b6e2dd589b1532124305baef95d
William Lin <wlin26@yahoo.com>
2020-12-06 13:22:59 -0500

    Added reading of Day_6 input text file

---
+++ Day_6/main.py
@@ -0,0 +1,28 @@
+import os
+
+def read_file(file_name: str) -> list[str]:
+    try:
+        data = []
+        temp_str = ''
+        read_file = open(file_name, 'r')
+        while True:
+            line = read_file.readline()
+            if not line:
+                break
+            if line.strip() == '':
+                data.append(temp_str)
+                temp_str = ''
+            else:
+                temp_str += line.strip()
+        if len(temp_str) > 0:
+            data.append(temp_str)
+        return data
+    except Exception as e:
+        print(str(e))
+
+def sum_unique_questions(question_str: str) -> int:
+    pass
+
+if __name__ == '__main__':
+    data = read_file(os.path.join(os.path.dirname(__file__), 'input.txt'))
+    print(data[:10])
