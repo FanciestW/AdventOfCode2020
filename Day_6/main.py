eef4e9fbf25061f33fee87dafee3923f90f45917
William Lin <wlin26@yahoo.com>
2020-12-06 19:33:49 -0500

    Completed Day 6 part 2 challenge

--- Day_6/main.py
+++ Day_6/main.py
@@ -13,7 +13,7 @@ def read_file(file_name: str) -> list:
                 data.append(temp_str_list)
                 temp_str_list = []
             else:
-                temp_str_list.append(line.strip())
+                temp_str_list.append(''.join(sorted(line.strip())))
         if len(temp_str_list) > 0:
             data.append(temp_str_list)
         return data
@@ -24,24 +24,20 @@ def sum_unique_questions(questions: list[str]) -> int:
     question_str = ''.join(q for q in questions)
     return len(list(set(question_str)))
 
-def sum_all_yes_questions(question_str: str) -> int:
-    question_dict = dict()
-    for c in question_str:
-        if c in question_dict:
-            question_dict[c] += 1
-        else:
-            question_dict[c] = 1
-    largest = None
-    for key in question_dict.keys():
-        if largest is None or question_dict[key] > question_dict[largest]:
-            largest = key
-    print(largest)
-    return question_dict[largest]
+def sum_all_yes_questions(questions: list[str]) -> int:
+    unique_questions = list(set(''.join(q for q in questions)))
+    count = 0
+    for c in unique_questions:
+        if all(c in question for question in questions):
+            count += 1
+    return count
 
 
 if __name__ == '__main__':
     data = read_file(os.path.join(os.path.dirname(__file__), 'input.txt'))
+    pass
     print(sum([sum_unique_questions(questions) for questions in data]))
     # > 6683
 
-    # print(sum_all_yes_questions(data[1]))
+    print(sum([sum_all_yes_questions(questions) for questions in data]))
+    # > 3122
