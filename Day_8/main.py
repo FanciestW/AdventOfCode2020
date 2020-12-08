6ab2d5d0613c0362579648af5d3971734b281675
William Lin <wlin26@yahoo.com>
2020-12-08 17:45:54 -0500

    WIP::Day 8 challenge part 2

--- Day_8/main.py
+++ Day_8/main.py
@@ -37,7 +37,56 @@ def find_accumulator_before_loop(instruction_list: list) -> int:
             break
     return accumulator
 
+def check_instructions(instruction_list: list, start_num: int) -> bool:
+    i = start_num
+    while i < len(instruction_list):
+        instr, num = instruction_list[i]
+        if instr == 'done':
+            return False
+        elif instr == 'acc' or instr == 'nop':
+            i += 1
+        elif instr == 'jmp':
+            i += num
+    return True
+
+def get_fixed_accumulator(instruction_list: list) -> int:
+    accumulator = 0
+    changed_one = False
+    i = 0
+    while i < len(instruction_list):
+        instr, num = instruction_list[i]
+        # Mark instruction as 'Done'
+        temp_instr = list(instruction_list[i])
+        temp_instr[0] = 'done'
+        instruction_list[i] = tuple(temp_instr)
+        if (instr == 'acc'):
+            accumulator += num
+            i += 1
+        elif (instr == 'jmp'):
+            if not changed_one and check_instructions(instruction_list, i + 1):
+                i += 1
+                changed_one = True
+            else:
+                i += num 
+        elif (instr == 'nop'):
+            if num != 0:
+                if not changed_one and check_instructions(instruction_list, i + num):
+                    i += num
+                    changed_one = True
+                else:
+                    i += 1
+            else:
+                i += 1
+        elif (instr == 'done'):
+            break
+        else:
+            print(f'Instruction `{instr}` not known')
+            break
+    return accumulator
+
 if __name__ == '__main__':
     data = read_data(os.path.join(os.path.dirname(__file__), 'input.txt'))
-    print(find_accumulator_before_loop(data))
+    # print(find_accumulator_before_loop(data))
     # > 1610
+
+    print(get_fixed_accumulator(data))
