0b0ec4d000146952bcb42538916a652b2fe6c6f3
William Lin <wlin26@yahoo.com>
2020-12-08 22:38:13 -0500

    Finished Day 8 challenge

--- Day_8/main.py
+++ Day_8/main.py
@@ -1,4 +1,5 @@
 import os
+import copy
 
 def read_data(file_name: str) -> list:
     try:
@@ -17,76 +18,49 @@ def read_data(file_name: str) -> list:
     except Exception as e:
         print(str(e))
 
-def find_accumulator_before_loop(instruction_list: list) -> int:
+def find_accumulator(instruction_list: list) -> (int, bool):
     accumulator = 0
     i = 0
+    seen_i = set()
     while i < len(instruction_list):
-        instr, num = instruction_list[i]
-        instruction_list[i] = ('done', 0)
-        if (instr == 'acc'):
-            accumulator += num
-            i += 1
-        elif (instr == 'jmp'):
-            i += num
-        elif (instr == 'nop'):
-            i += 1
-        elif (instr == 'done'):
-            break
+        if i in seen_i:
+            return accumulator, False
         else:
-            print(f'Instruction `{instr}` not known')
-            break
-    return accumulator
-
-def check_instructions(instruction_list: list, start_num: int) -> bool:
-    i = start_num
-    while i < len(instruction_list):
+            seen_i.add(i)
         instr, num = instruction_list[i]
-        if instr == 'done':
-            return False
-        elif instr == 'acc' or instr == 'nop':
+        if instr == 'acc':
+            accumulator += num
             i += 1
         elif instr == 'jmp':
             i += num
-    return True
+        elif instr == 'nop':
+            i += 1
+    return accumulator, True
 
 def get_fixed_accumulator(instruction_list: list) -> int:
-    accumulator = 0
-    changed_one = False
     i = 0
     while i < len(instruction_list):
         instr, num = instruction_list[i]
-        # Mark instruction as 'Done'
-        temp_instr = list(instruction_list[i])
-        temp_instr[0] = 'done'
-        instruction_list[i] = tuple(temp_instr)
-        if (instr == 'acc'):
-            accumulator += num
-            i += 1
-        elif (instr == 'jmp'):
-            if not changed_one and check_instructions(instruction_list, i + 1):
-                i += 1
-                changed_one = True
-            else:
-                i += num 
-        elif (instr == 'nop'):
-            if num != 0:
-                if not changed_one and check_instructions(instruction_list, i + num):
-                    i += num
-                    changed_one = True
-                else:
-                    i += 1
-            else:
-                i += 1
-        elif (instr == 'done'):
+        if instr == 'jmp':
+            new_instr = ('nop', num)
+            instruction_list[i] = new_instr
+        elif instr == 'nop':
+            new_instr = ('jmp', num)
+            instruction_list[i] = new_instr
+        accum_res, hasNoCycles = find_accumulator(instruction_list)
+        if hasNoCycles:
+            return accum_res
             break
         else:
-            print(f'Instruction `{instr}` not known')
-            break
-    return accumulator
+            instruction_list[i] = (instr, num)
+            i += 1
+    return None
+
 
 if __name__ == '__main__':
     data = read_data(os.path.join(os.path.dirname(__file__), 'input.txt'))
-    # print(find_accumulator_before_loop(data))
+    print(find_accumulator(data)[0])
     # > 1610
 
     print(get_fixed_accumulator(data))
+    # > 1703
