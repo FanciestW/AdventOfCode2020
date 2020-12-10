b46e4c6899e160f49b29b88f9c0645929606d28f
William Lin <wlin26@yahoo.com>
2020-12-09 19:11:53 -0500

    Completed Day 9 Part 1

--- Day_9/main.py
+++ Day_9/main.py
@@ -16,12 +16,24 @@ def read_data(file_name: str) -> List[int]:
     except Exception as e:
         print(str(e))
 
+def hasPair(nums: List[int], target: int) -> bool:
+    for i, n in enumerate(nums):
+        target_num = target - n
+        new_list = nums[:i] + nums[i+1:]
+        if target_num in new_list:
+            return True
+    return False
+
 def check_rule(data: List[int], pre_count=25) -> int:
     for i in range(pre_count, len(data)):
         num = data[i]
         preamble_data = data[i-pre_count:i]
-        print(i, preamble_data)
+        if not hasPair(preamble_data, num):
+            return num
+    return None
+
 
 if __name__ == '__main__':
-    data = read_data(os.path.join(os.path.dirname(__file__), 'test_input.txt'))
-    check_rule(data, pre_count=5)
+    data = read_data(os.path.join(os.path.dirname(__file__), 'input.txt'))
+    print(check_rule(data, pre_count=25))
+    # > 2089807806
