8ae5c5729440c7534f829e3164595ec5e7f50e9a
William Lin <wlin26@yahoo.com>
2020-12-10 20:28:43 -0500

    Finished Day 10 part 1

--- Day_10/main.py
+++ Day_10/main.py
@@ -15,6 +15,26 @@ def read_file(file_name: str) -> List[int]:
     except Exception as e:
         print(str(e))
 
+def count_jolt_diff(data: List[int]) -> int:
+    data.sort()
+    print(data) # TODO::Remove me
+    
+    j1_count = 0
+    j3_count = 0
+    last_adapter = 0 # Wall socket starts at 0 jolts
+    for n in data:
+        jolt_diff = n - last_adapter
+        if jolt_diff == 1:
+            j1_count += 1
+        elif jolt_diff == 3:
+            j3_count += 1
+        else:
+            print(f'Last Adapter: {last_adapter}J, Bad Adapter: {n}J')
+        last_adapter = n
+    j3_count += 1 # Device adapter is always 3 jolt difference
+    return j1_count * j3_count
+
 if __name__ == '__main__':
     data = read_file(os.path.join(os.path.dirname(__file__), 'input.txt'))
-    print(data)
+    print(count_jolt_diff(data))
+    # > 1890
