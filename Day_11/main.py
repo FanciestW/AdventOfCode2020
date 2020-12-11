ebd628637697fb38657b242d207f96ccfd1942c2
William Lin <wlin26@yahoo.com>
2020-12-11 17:48:26 -0500

    Fixed test input

--- Day_11/main.py
+++ Day_11/main.py
@@ -1,7 +1,7 @@
 import os
 from typing import List
 
-def read_file(file_name: str) -> List[List[int]]:
+def read_file(file_name: str) -> List[List[str]]:
     try:
         data = list()
         read_file = open(file_name, 'r')
@@ -15,6 +15,10 @@ def read_file(file_name: str) -> List[List[int]]:
         print(str(e))
         return []
 
+def part_1(data: List[List[str]]):
+    neighbor_count = [[0 if c == 'L' else -1 for c in row] for row in data]
+    print(neighbor_count)
+
 if __name__ == '__main__':
-    data = read_file(os.path.join(os.path.dirname(__file__), 'input.txt'))
+    data = read_file(os.path.join(os.path.dirname(__file__), 'test_input.txt'))
     print(data)
