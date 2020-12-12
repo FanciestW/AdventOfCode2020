5ed0967b88c1631a1c90cb1ef280c5e9a49ab65b
William Lin <wlin26@yahoo.com>
2020-12-11 21:41:10 -0500

    WIP Part 1 of Day 11

--- Day_11/main.py
+++ Day_11/main.py
@@ -1,5 +1,7 @@
 import os
 from typing import List
+import itertools
+import copy
 
 def read_file(file_name: str) -> List[List[str]]:
     try:
@@ -15,10 +17,42 @@ def read_file(file_name: str) -> List[List[str]]:
         print(str(e))
         return []
 
-def part_1(data: List[List[str]]):
-    neighbor_count = [[0 if c == 'L' else -1 for c in row] for row in data]
-    print(neighbor_count)
+def pretty_print(data: list):
+    for row in data:
+        if type(row[0]) is str:
+            print(''.join(row))
+        else:
+            print(row)
+
+def part_1(data: List[List[str]], rounds=3) -> int:
+    seating = [['#' if c != '.' else '.' for c in row] for row in data]
+    for i in range(rounds + 1):
+        seating = seat_move(seating)
+    return [c for row in seating for c in row].count('#')
+    
+def seat_move(data: List[List[str]]) -> List[List[str]]:
+    new_seat_map = copy.deepcopy(data)
+    for i, row in enumerate(data):
+        for j, c in enumerate(row):
+            if c not in ['L', '#']:
+                new_seat_map[i][j] = '.'
+                continue
+            row_i = [i + n for n in range (-1, 2) if i + n >= 0 and i + n < len(data)]
+            col_i = [j + n for n in range (-1, 2) if j + n >= 0 and j + n < len(row)]
+            coord = [(x, y) for x, y in list(itertools.product(row_i, col_i)) if not (x == i and y == j)]
+            neighbors_count = [data[x][y] for x, y in coord].count('#')
+            if neighbors_count >= 4:
+                new_seat_map[i][j] = 'L'
+            elif neighbors_count == 0 and c == 'L':
+                new_seat_map[i][j] = '#'
+            else:
+                new_seat_map[i][j] = c
+    return new_seat_map
+    
+    
 
 if __name__ == '__main__':
-    data = read_file(os.path.join(os.path.dirname(__file__), 'test_input.txt'))
-    print(data)
+    data = read_file(os.path.join(os.path.dirname(__file__), 'input.txt'))
+    
+    print(part_1(data))
+    # > 2270
