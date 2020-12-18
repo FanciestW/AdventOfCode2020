6a3e7087843072028981f71831bdac25a544ec42
William Lin <wlin26@yahoo.com>
2020-12-18 13:49:16 -0500

    Finished part 1 of Day 18 challenge

--- Day_18/main.py
+++ Day_18/main.py
@@ -1,4 +1,5 @@
 import os
+import re
 from typing import List
 
 def read_file(file_name: str) -> List[str]:
@@ -11,11 +12,42 @@ def read_file(file_name: str) -> List[str]:
                 break
             else:
                 line = line.strip()
-            data.append(line.replace(' ', ''))
+            data.append(line)
         return data
     except Exception as e:
         print(str(e))
 
+def eval_equation(eq: str) -> int:
+    try:
+        eq = eq.replace(' ', '')
+        if eq.count('(') != eq.count(')'):
+            raise ValueError('Bad parentheses!')
+        while eq.count('(') > 0:
+            stack = list()
+            for i, c in enumerate(eq):
+                if c == '(':
+                    stack.append(i)
+                elif c == ')':
+                    start_i = stack.pop() + 1
+                    end_i = i
+                    inner_eq = eq[start_i:end_i]
+                    inner_res = str(eval_equation(inner_eq))
+                    eq = eq.replace(f'({inner_eq})', inner_res, 1)
+                    break
+        # No more inner equations wrapped in parentheses:
+        while eq.count('*') > 0 or eq.count('+') > 0:
+            eval_str = re.findall('(\d+[*+]{1}\d+)', eq)[0]
+            eval_res = eval(eval_str)
+            eq = eq.replace(eval_str, str(eval_res), 1)
+        return int(eq)
+    except Exception as e:
+        print(str(e))
+
+def part_1(data: List[str]) -> None:
+    eval_res = [eval_equation(eq) for eq in data]
+    print(sum(eval_res))
+
 if __name__ == '__main__':
     data = read_file(os.path.join(os.path.dirname(__file__), 'input.txt'))
-    print(data)
+    part_1(data)
+    # > 5783053349377
