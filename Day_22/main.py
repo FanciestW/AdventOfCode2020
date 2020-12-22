2d1ebcd07232708618ac84a375df3eebe218ab17
William Lin <wlin26@yahoo.com>
2020-12-22 18:48:49 -0500

    Completed Day 22 part 1

--- Day_22/main.py
+++ Day_22/main.py
@@ -25,7 +25,24 @@ def read_file(file_name: str) -> (List[int], List[int]):
     except Exception as e:
         print(str(e))
 
+# Returns winning players score as int
+def play_game(p1_cards: List[int], p2_cards: List[int]) -> int:
+    while len(p1_cards) > 0 and len(p2_cards) > 0:
+        p1 = p1_cards.pop(0)
+        p2 = p2_cards.pop(0)
+        if p1 > p2:
+            p1_cards += [p1, p2]
+        elif p2 > p1:
+            p2_cards += [p2, p1]
+        else:
+            print('A tie occurred')
+    winner_cards = p1_cards if len(p1_cards) > 0 else p2_cards
+    winning_sum = sum([(i + 1) * n for i, n in enumerate(winner_cards[::-1])])
+    return winning_sum
+
 if __name__ == '__main__':
     p1_cards, p2_cards = read_file(os.path.join(os.path.dirname(__file__), 'input.txt'))
-    print(p1_cards)
-    print(p2_cards)
+
+    # Part 1:
+    print(play_game(p1_cards, p2_cards))
+    # > 32179
