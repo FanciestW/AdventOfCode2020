16a2896e31c0d4095fa04227cfb52182ba1516cb
William Lin <wlin26@yahoo.com>
2020-12-21 22:13:08 -0500

    Added building of ingredient dict

--- Day_21/main.py
+++ Day_21/main.py
@@ -19,5 +19,22 @@ def read_file(file_name: str) -> List[tuple]:
     except Exception as e:
         print(str(e))
 
+def build_ingredient_dict(data: List[tuple]) -> dict:
+    ingredient_allergen_dict = dict()
+    for ingredient_list, allergen_list in data:
+        for ingredient in ingredient_list:
+            for allergen in allergen_list:
+                if ingredient in ingredient_allergen_dict and allergen in ingredient_allergen_dict[ingredient]:
+                    ingredient_allergen_dict[ingredient][allergen] += 1
+                elif ingredient in ingredient_allergen_dict:
+                    ingredient_allergen_dict[ingredient][allergen] = 1
+                else:
+                    ingredient_allergen_dict[ingredient] = { allergen: 1 }
+    return ingredient_allergen_dict
+
+
 if __name__ == '__main__':
-    data = read_file(os.path.join(os.path.dirname(__file__), 'input.txt'))
+    data = read_file(os.path.join(os.path.dirname(__file__), 'test_input.txt'))
+
+    allergen_dict = build_ingredient_dict(data)
+    print(allergen_dict)
