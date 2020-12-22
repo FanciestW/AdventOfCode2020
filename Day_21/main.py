c5bd964377e69b0d3ebfdf6ec33d1ba127f171f5
William Lin <wlin26@yahoo.com>
2020-12-21 23:33:16 -0500

    Working on Day 21

--- Day_21/main.py
+++ Day_21/main.py
@@ -1,6 +1,7 @@
 import os
 import re
 from typing import List
+import pprint
 
 def read_file(file_name: str) -> List[tuple]:
     try:
@@ -19,22 +20,32 @@ def read_file(file_name: str) -> List[tuple]:
     except Exception as e:
         print(str(e))
 
-def build_ingredient_dict(data: List[tuple]) -> dict:
-    ingredient_allergen_dict = dict()
+def build_data_struct(data: List[tuple]) -> (dict, list):
+    ingredient_set = set()
+    allergen_dict = dict()
     for ingredient_list, allergen_list in data:
-        for ingredient in ingredient_list:
-            for allergen in allergen_list:
-                if ingredient in ingredient_allergen_dict and allergen in ingredient_allergen_dict[ingredient]:
-                    ingredient_allergen_dict[ingredient][allergen] += 1
-                elif ingredient in ingredient_allergen_dict:
-                    ingredient_allergen_dict[ingredient][allergen] = 1
-                else:
-                    ingredient_allergen_dict[ingredient] = { allergen: 1 }
-    return ingredient_allergen_dict
+        ingredient_set.update(ingredient_list)
+        for allergen in allergen_list:
+            if allergen in allergen_dict:
+                allergen_dict[allergen].update(ingredient_list)
+            else:
+                allergen_dict[allergen] = set(ingredient_list)
+    return allergen_dict, list(ingredient_set)
+
+def find_no_allergens(allergen_dict: dict, ingredient_list: list) -> List[str]:
+    allergen_list = allergen_dict.keys()
+    unique_allergens = dict()
+    for allergen in allergen_dict:
+        for a in [a for a in allergen_list if a != allergen]:
+            allergen_dict[allergen] = allergen_dict[allergen].difference(allergen_dict[a])
+    print(unique_allergens)
 
 
 if __name__ == '__main__':
     data = read_file(os.path.join(os.path.dirname(__file__), 'test_input.txt'))
 
-    allergen_dict = build_ingredient_dict(data)
-    print(allergen_dict)
+    # Part 1:
+    allergen_dict, ingredient_list = build_data_struct(data)
+    pprint.pp(allergen_dict)
+    pprint.pp(ingredient_list)
+    no_allergens = find_no_allergens(allergen_dict, ingredient_list)
