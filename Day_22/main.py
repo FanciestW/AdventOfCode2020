35736440c29eb6cf57cfe6050227d92a22ac35f3
William Lin <wlin26@yahoo.com>
2020-12-22 23:18:26 -0500

    Completed Day 22 Challenges

--- Day_22/main.py
+++ Day_22/main.py
@@ -1,5 +1,6 @@
 import os
 from typing import List
+from math import prod
 
 def read_file(file_name: str) -> (List[int], List[int]):
     try:
@@ -25,24 +26,41 @@ def read_file(file_name: str) -> (List[int], List[int]):
     except Exception as e:
         print(str(e))
 
-# Returns winning players score as int
-def play_game(p1_cards: List[int], p2_cards: List[int]) -> int:
+# Returns winning players score as int and a bool which is True if player 1 wins
+def play_game(p1_cards: List[int], p2_cards: List[int], recursive_mode=True) -> (int, bool):
+    p1_seen_set = set()
+    p2_seen_set = set()
     while len(p1_cards) > 0 and len(p2_cards) > 0:
+        p1_deck, p2_deck = tuple(p1_cards), tuple(p2_cards)
+        if p1_deck in p1_seen_set or p2_deck in p2_seen_set:
+            return (-1, True)
+        else:
+            p1_seen_set.add(p1_deck)
+            p2_seen_set.add(p2_deck)
+
         p1 = p1_cards.pop(0)
         p2 = p2_cards.pop(0)
-        if p1 > p2:
-            p1_cards += [p1, p2]
-        elif p2 > p1:
-            p2_cards += [p2, p1]
+        p1_win = False
+        if recursive_mode and (p1 <= len(p1_cards) and p2 <= len(p2_cards)):
+            p1_win = play_game(p1_cards[:p1], p2_cards[:p2], recursive_mode=True)[1]
         else:
-            print('A tie occurred')
+            p1_win = p1 > p2
+
+        if p1_win:
+            p1_cards += [p1, p2]
+        else:
+            p2_cards += [p2, p1]
+    p1_win = len(p1_cards) > 0
     winner_cards = p1_cards if len(p1_cards) > 0 else p2_cards
-    winning_sum = sum([(i + 1) * n for i, n in enumerate(winner_cards[::-1])])
-    return winning_sum
+    return sum([(i + 1) * n for i, n in enumerate(winner_cards[::-1])]), p1_win
 
 if __name__ == '__main__':
     p1_cards, p2_cards = read_file(os.path.join(os.path.dirname(__file__), 'input.txt'))
 
     # Part 1:
-    print(play_game(p1_cards, p2_cards))
+    print(play_game(p1_cards, p2_cards, recursive_mode=False)[0])
     # > 32179
+
+    # Part 2:
+    print(play_game(p1_cards, p2_cards, recursive_mode=True)[0])
+    # > 30498
