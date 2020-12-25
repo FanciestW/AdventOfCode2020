95baad973c7bb9bbe4dc2fdfd728afbbce5e6655
William Lin <wlin26@yahoo.com>
2020-12-24 20:15:13 -0500

    Completed part 1 of Day 24 challenge

--- Day_24/main.py
+++ Day_24/main.py
@@ -1,7 +1,8 @@
 import os
+import re
 from typing import List
 
-def read_file(filename: str) -> List[str]:
+def read_file(filename: str) -> List[List[str]]:
     try:
         data = list()
         read_file = open(filename, 'r')
@@ -11,11 +12,40 @@ def read_file(filename: str) -> List[str]:
                 break
             else:
                 line = line.strip()
-            data.append(line)
+            data.append(re.findall('[ns][ew]{1}|[ew]{1}', line))
         return data
     except Exception as e:
         print(str(e))
 
+def part1(data: List[List[str]]) -> int:
+    tiles = dict()
+    for line in data:
+        x, y = 0, 0
+        for dir in line:
+            if dir == 'e':
+                x += 2
+            elif dir == 'se':
+                x += 1
+                y -= 1
+            elif dir == 'sw':
+                x -= 1
+                y -= 1
+            elif dir == 'w':
+                x -= 2
+            elif dir == 'nw':
+                x -= 1
+                y += 1
+            elif dir == 'ne':
+                x += 1
+                y += 1
+        pos = f'{x},{y}'
+        if pos in tiles:
+            tiles[pos] = not tiles[pos]
+        else:
+            tiles[pos] = True
+    return list(tiles.values()).count(True)
+
+
 if __name__ == '__main__':
     data = read_file(os.path.join(os.path.dirname(__file__), 'input.txt'))
-    print(data[:3])
+    print(part1(data))
